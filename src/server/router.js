c9aa27d2b89a4a301b4e81887a93872dc97537b7
safaaamro <safaaamro072@gmail.com>
2018-12-04 14:04:57 +0200

    create server #4

---
+++ src/server/router.js
@@ -0,0 +1,24 @@
+
+const handlers = require('./handlers');
+
+
+const router = (request, response) => {
+  if (request.method === 'GET') {
+    if (request.url === '/') {
+      handlers.homeHandler(request, response);
+    } else if (request.url.includes('/search?value=')) {
+      handlers.searchMoviesHandler(request, response);
+    } else if (request.url.includes('/getMovies?id=')) {
+      handlers.getMoviesHandler(request, response);
+    }else if (request.url.includes('/PostMovies?id=')) {
+      handlers.postMoviesHandler(request, response);
+    } else if (request.url.split('.')[1]) {
+      handlers.publicHandler(request, response);
+    } else {
+      handlers.errorHandler(request, response);
+    }
+  }
+};
+
+
+module.exports = router;
