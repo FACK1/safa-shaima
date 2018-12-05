270510a73c0c33cb9648f959b7c5507b94a1d331
shaima96 <141062@ppu.edu.ps>
2018-12-05 14:18:14 -0600

    added datbase url in heroku #19

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -1,14 +1,13 @@
-const {Pool} =require("pg");
-require("env2")('.env');
+const { Pool } = require('pg')
+require('env2')('.env')
 
-const {DB_URL} = process.env;
-console.log(DB_URL);
-if(! DB_URL){
-  throw Error ("No database URL")
+const { DATABASE_URL } = process.env
+if (!DATABASE_URL) {
+  throw Error('No database URL')
 }
 
 const option = {
-  connectionString: DB_URL
-};
+  connectionString: DATABASE_URL
+}
 
-module.exports = new Pool(option);
+module.exports = new Pool(option)
