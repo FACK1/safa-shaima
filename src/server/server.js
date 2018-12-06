fd08cdc76f850fcbf2d1166f48c420d01ec77720
safaaamro <safaaamro072@gmail.com>
2018-12-06 08:38:35 +0200

    frontend update

--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,9 @@ require('env2')('.../.env');
 
 const http = require('http');
 const router = require('./router.js');
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5001;
 const server = http.createServer(router);
 
 server.listen(PORT, () => {
-  console.log('server is up and running on port 4000!');
+  console.log('server is up and running on port 5001!');
 });
