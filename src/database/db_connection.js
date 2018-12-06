5b900746d4b0ccf31937aa40da8df1f0bdec07f6
shaima96 <141062@ppu.edu.ps>
2018-12-06 11:19:29 -0600

    fix travis problem

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -3,17 +3,17 @@ const url = require('url')
 
 require('env2')('.env')
 
-const { DB_URL, DATABASE_URL } = process.env
+const { DB_URL } = process.env
 
-if(!DB_URL || DATABASE_URL){
+if(!DB_URL){
   throw Error('No Data Base URL');
 }
 
 const option = {
-  connectionString: DB_URL || DATABASE_URL
+  connectionString: DB_URL
 }
 
-const hostname = url.parse(process.env.DATABASE_URL).hostname;
+const hostname = url.parse(process.env.DB_URL).hostname;
 
 option.ssl = (hostname !== 'localhost');
 module.exports = new Pool(option)
